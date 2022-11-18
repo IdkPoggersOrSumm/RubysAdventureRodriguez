0df66a49403b8168ab6bf3a473a87eb87c1117bc
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2022-11-18 11:40:33 -0800

    Added New Features
    Added collider to ruby, box, and started on health collector, added collider with water, and added physics/

--- RubysAdventureRodriguez/Assets/Scripts/RubyController.cs
+++ RubysAdventureRodriguez/Assets/Scripts/RubyController.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 10;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
         Debug.Log(horizontal);
-        Vector2 position = transform.position;
-                                //3.0f for faster ruby movement
-                                //0.1f for REALLY slow movement
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        //3.0f for faster ruby movement
+        //0.1f for REALLY slow movement
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
-        
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
     }
 }
