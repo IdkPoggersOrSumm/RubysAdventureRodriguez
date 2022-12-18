602162edb53073b8f05cfcc65579b05ce0b1d1bf
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2022-12-17 21:27:47 -0800

    Finished Rubys Adventure

--- RubysAdventureRodriguez/Assets/Scripts/RubyController.cs
+++ RubysAdventureRodriguez/Assets/Scripts/RubyController.cs
@@ -6,35 +6,39 @@ using UnityEngine.WSA;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
+    
     public int maxHealth = 5;
-
+    
     public GameObject projectilePrefab;
-
-    public int health { get { return currentHealth; } }
+    
+    public AudioClip throwSound;
+    public AudioClip hitSound;
+    
+    public int health { get { return currentHealth; }}
     int currentHealth;
-
+    
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
-
+    
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-
+    
     Animator animator;
-    Vector2 lookDirection = new Vector2(1, 0);
-
-   
+    Vector2 lookDirection = new Vector2(1,0);
+    
+    AudioSource audioSource;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //QualitySettings.vSyncCount = 0;
-        //Application.targetFrameRate = 10;
+        animator = GetComponent<Animator>();
+        
         currentHealth = maxHealth;
-        //currentHealth = 1;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -42,58 +46,73 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
+        
         Vector2 move = new Vector2(horizontal, vertical);
-
-        if (!Mathf.Approximately(move.x, move.y) || !Mathf.Approximately(move.y, 0.0f))
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
             lookDirection.Normalize();
         }
-
+        
         animator.SetFloat("Look X", lookDirection.x);
         animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-         
-        //Debug.Log(horizontal);
+        
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
         }
-        if (Input.GetKeyDown(KeyCode.C))
+        
+        if(Input.GetKeyDown(KeyCode.C))
         {
             Launch();
         }
+        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
+    
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        //3.0f for faster ruby movement
-        //0.1f for REALLY slow movement
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
         rigidbody2d.MovePosition(position);
     }
+
     public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
                 return;
-
+            
             isInvincible = true;
             invincibleTimer = timeInvincible;
+            animator.SetTrigger("Hit");
 
+            PlaySound(hitSound);
         }
+        
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
-
-     UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth); 
-
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
+    
     void Launch()
     {
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
@@ -102,5 +121,12 @@ public class RubyController : MonoBehaviour
         projectile.Launch(lookDirection, 300);
 
         animator.SetTrigger("Launch");
+        
+        PlaySound(throwSound);
+    } 
+    
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 }
